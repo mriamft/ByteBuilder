cc4a254974d0e42b23c33c6eeb2c7ef0754fe9f2
mriamft <129700412+mriamft@users.noreply.github.com>
2023-11-18 23:20:38 +0300

    rwd

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.menu-button');
+    const navbar = document.querySelector('.navbar');
+  
+    menuButton.addEventListener('click', () => {
+      navbar.classList.toggle('active');
+    });
+  });
